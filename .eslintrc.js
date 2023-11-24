0a74fa2e310eb4650d98097b177c70b7cfb3ed00
Kshitiz Karki <karki.kshitiz.23@gmail.com>
2023-11-24 19:06:04 +0545

    Add new test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    'node': true
+    'node': true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -17,7 +18,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
@@ -46,8 +47,5 @@ module.exports = {
     ],
     "no-console": "error",
     "react/prop-types": 0
-  },
-  "globals": {
-    "cy": true
   }
 }
